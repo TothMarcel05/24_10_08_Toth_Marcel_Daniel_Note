246f7084e7b460ac33b5dfd2fd53dd1ce1d5407e
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2024-10-08 10:39:14 +0200

    Add app.py

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    if request.method == 'POST':
+        name = request.form.get('product_name')
+    return render_template('index.html', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
